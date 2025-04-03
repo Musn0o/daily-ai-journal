e76ce6b9b113254c7f4550ee1e87969f7e10a986
scar <askarmoustafa@yahoo.com>
2025-04-03 08:41:54 +0200

    Added waiter.py

--- Gen_AI_Intensive_Course/Day-03/waiter.py
+++ Gen_AI_Intensive_Course/Day-03/waiter.py
@@ -1,16 +1,15 @@
-from google import genai
-from langgraph.graph.message import add_messages
 import os
-from typing import Annotated
+from langgraph.graph.message import add_messages
+from typing import Annotated, Literal
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langchain_google_genai import ChatGoogleGenerativeAI
-from IPython.display import Image, display
-from pprint import pprint
 from langchain_core.messages.ai import AIMessage
-from typing import Literal
 from langchain_core.tools import tool
 from langgraph.prebuilt import ToolNode
+from collections.abc import Iterable
+from random import randint
+from langchain_core.messages.tool import ToolMessage
 
 
 GEMINI_API_KEY = os.environ.get("GOOGLE_API_KEY")
@@ -37,7 +36,7 @@ class OrderState(TypedDict):
 # as tone and what is permitted for discussion.
 WAITERBOT_SYSINT = (
     "system",  # 'system' indicates the message is a system instruction.
-    "You are a WaiterBot, an interactive Bakery ordering system. A human will talk to you about the "
+    "You are a WaiterBot, an interactive Bakery ordering system in Scar's Bakery. A human will talk to you about the "
     "available products you have and you will answer any questions about menu items (and only about "
     "menu items - no off-topic discussion, but you can chat about the products and their history). "
     "The customer will place an order for 1 or more items from the menu, which you will structure "
@@ -59,132 +58,7 @@ WAITERBOT_SYSINT = (
 )
 
 # This is the message with which the system opens the conversation.
-WELCOME_MSG = "Welcome to Scar's Bakery. Type `bye` to quit. How may I serve you today?"
-
-
-# Try using different models. The Gemini 2.0 flash model is highly
-# capable, great with tools, and has a generous free tier. If you
-# try the older 1.5 models, note that the `pro` models are better at
-# complex multi-tool cases like this, but the `flash` models are
-# faster and have more free quota.
-# Check out the features and quota differences here:
-#  - https://ai.google.dev/gemini-api/docs/models/gemini
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
-
-
-def chatbot(state: OrderState) -> OrderState:
-    """The chatbot itself. A simple wrapper around the model's own chat interface."""
-    message_history = [WAITERBOT_SYSINT] + state["messages"]
-    return {"messages": [llm.invoke(message_history)]}
-
-
-# Set up the initial graph based on our state definition.
-graph_builder = StateGraph(OrderState)
-
-# Add the chatbot function to the app graph as a node called "chatbot".
-graph_builder.add_node("chatbot", chatbot)
-
-# Define the chatbot node as the app entrypoint.
-graph_builder.add_edge(START, "chatbot")
-
-chat_graph = graph_builder.compile()
-
-Image(chat_graph.get_graph().draw_mermaid_png())
-
-
-user_msg = "Hello, what can you do?"
-state = chat_graph.invoke({"messages": [user_msg]})
-
-# The state object contains lots of information. Uncomment the pprint lines to see it all.
-pprint(state)
-
-# Note that the final state now has 2 messages. Our HumanMessage, and an additional AIMessage.
-for msg in state["messages"]:
-    print(f"{type(msg).__name__}: {msg.content}")
-
-
-user_msg = "Oh great, what kinds of croissant can you make?"
-
-state["messages"].append(user_msg)
-state = chat_graph.invoke(state)
-
-pprint(state)
-for msg in state["messages"]:
-    print(f"{type(msg).__name__}: {msg.content}")
-
-
-def human_node(state: OrderState) -> OrderState:
-    """Display the last model message to the user, and receive the user's input."""
-    last_msg = state["messages"][-1]
-    print("Model:", last_msg.content)
-
-    user_input = input("User: ")
-
-    # If it looks like the user is trying to quit, flag the conversation
-    # as over.
-    if user_input in {"q", "quit", "exit", "goodbye"}:
-        state["finished"] = True
-
-    return state | {"messages": [("user", user_input)]}
-
-
-def chatbot_with_welcome_msg(state: OrderState) -> OrderState:
-    """The chatbot itself. A wrapper around the model's own chat interface."""
-
-    if state["messages"]:
-        # If there are messages, continue the conversation with the Gemini model.
-        new_output = llm.invoke([WAITERBOT_SYSINT] + state["messages"])
-    else:
-        # If there are no messages, start with the welcome message.
-        new_output = AIMessage(content=WELCOME_MSG)
-
-    return state | {"messages": [new_output]}
-
-
-# Start building a new graph.
-graph_builder = StateGraph(OrderState)
-
-# Add the chatbot and human nodes to the app graph.
-graph_builder.add_node("chatbot", chatbot_with_welcome_msg)
-graph_builder.add_node("human", human_node)
-
-# Start with the chatbot again.
-graph_builder.add_edge(START, "chatbot")
-
-# The chatbot will always go to the human next.
-graph_builder.add_edge("chatbot", "human")
-
-
-def maybe_exit_human_node(state: OrderState) -> Literal["chatbot", "__end__"]:
-    """Route to the chatbot, unless it looks like the user is exiting."""
-    if state.get("finished", False):
-        return END
-    else:
-        return "chatbot"
-
-
-graph_builder.add_conditional_edges("human", maybe_exit_human_node)
-
-chat_with_human_graph = graph_builder.compile()
-
-Image(chat_with_human_graph.get_graph().draw_mermaid_png())
-
-
-# The default recursion limit for traversing nodes is 25 - setting it higher means
-# you can try a more complex order with multiple steps and round-trips (and you
-# can chat for longer!)
-config = {"recursion_limit": 100}
-
-# Remember that this will loop forever, unless you input `q`, `quit` or one of the
-# other exit terms defined in `human_node`.
-# Uncomment this line to execute the graph:
-state = chat_with_human_graph.invoke({"messages": []}, config)
-
-# Things to try:
-#  - Just chat! There's no ordering or menu yet.
-#  - 'q' to exit.
-
-pprint(state)
+WELCOME_MSG = "Welcome to Scar's Bakery. Type `Bye` to quit. How may I serve you today?"
 
 
 @tool
@@ -242,27 +116,7 @@ def get_menu() -> str:
     """
 
 
-# Define the tools and create a "tools" node.
-tools = [get_menu]
-tool_node = ToolNode(tools)
-
-# Attach the tools to the model so that it knows what it can call.
-llm_with_tools = llm.bind_tools(tools)
-
-
-def maybe_route_to_tools(state: OrderState) -> Literal["tools", "human"]:
-    """Route between human or tool nodes, depending if a tool call is made."""
-    if not (msgs := state.get("messages", [])):
-        raise ValueError(f"No messages found when parsing state: {state}")
-
-    # Only route based on the last message.
-    msg = msgs[-1]
-
-    # When the chatbot returns tool_calls, route to the "tools" node.
-    if hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
-        return "tools"
-    else:
-        return "human"
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 
 def chatbot_with_tools(state: OrderState) -> OrderState:
@@ -279,22 +133,188 @@ def chatbot_with_tools(state: OrderState) -> OrderState:
     return defaults | state | {"messages": [new_output]}
 
 
+def human_node(state: OrderState) -> OrderState:
+    """Display the last model message to the user, and receive the user's input."""
+    last_msg = state["messages"][-1]
+    print("Model:", last_msg.content)
+
+    user_input = input("User: ")
+
+    # If it looks like the user is trying to quit, flag the conversation
+    # as over.
+    if user_input in {"bye", "Bye", "quit", "exit", "goodbye"}:
+        state["finished"] = True
+
+    return state | {"messages": [("user", user_input)]}
+
+
+def order_node(state: OrderState) -> OrderState:
+    """The ordering node. This is where the order state is manipulated."""
+    tool_msg = state.get("messages", [])[-1]
+    order = state.get("order", [])
+    outbound_msgs = []
+    order_placed = False
+
+    for tool_call in tool_msg.tool_calls:
+        if tool_call["name"] == "add_to_order":
+            # Each order item is just a string. This is where it assembled as "drink (modifiers, ...)".
+            modifiers = tool_call["args"]["modifiers"]
+            modifier_str = ", ".join(modifiers) if modifiers else "no modifiers"
+
+            order.append(f"{tool_call['args']['item']} ({modifier_str})")
+            response = "\n".join(order)
+
+        elif tool_call["name"] == "confirm_order":
+            # We could entrust the LLM to do order confirmation, but it is a good practice to
+            # show the user the exact data that comprises their order so that what they confirm
+            # precisely matches the order that goes to the kitchen - avoiding hallucination
+            # or reality skew.
+
+            # In a real scenario, this is where you would connect your POS screen to show the
+            # order to the user.
+
+            print("Your order:")
+            if not order:
+                print("  (no items)")
+
+            for drink in order:
+                print(f"  {drink}")
+
+            response = input("Is this correct? ")
+
+        elif tool_call["name"] == "get_order":
+            response = "\n".join(order) if order else "(no order)"
+
+        elif tool_call["name"] == "clear_order":
+            order.clear()
+            response = None
+
+        elif tool_call["name"] == "place_order":
+            order_text = "\n".join(order)
+            print("Sending order to kitchen!")
+            print(order_text)
+
+            # TODO(you!): Implement bakery order processing.
+            order_placed = True
+            response = randint(1, 5)  # ETA in minutes
+
+        else:
+            raise NotImplementedError(f"Unknown tool call: {tool_call['name']}")
+
+        # Record the tool results as tool messages.
+        outbound_msgs.append(
+            ToolMessage(
+                content=response,
+                name=tool_call["name"],
+                tool_call_id=tool_call["id"],
+            )
+        )
+
+    return {"messages": outbound_msgs, "order": order, "finished": order_placed}
+
+
+def maybe_route_to_tools(state: OrderState) -> str:
+    """Route between chat and tool nodes if a tool call is made."""
+    if not (msgs := state.get("messages", [])):
+        raise ValueError(f"No messages found when parsing state: {state}")
+
+    msg = msgs[-1]
+
+    if state.get("finished", False):
+        # When an order is placed, exit the app. The system instruction indicates
+        # that the chatbot should say thanks and goodbye at this point, so we can exit
+        # cleanly.
+        return END
+
+    elif hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
+        # Route to `tools` node for any automated tool calls first.
+        if any(
+            tool["name"] in tool_node.tools_by_name.keys() for tool in msg.tool_calls
+        ):
+            return "tools"
+        else:
+            return "ordering"
+
+    else:
+        return "human"
+
+
+def maybe_exit_human_node(state: OrderState) -> Literal["chatbot", "__end__"]:
+    """Route to the chatbot, unless it looks like the user is exiting."""
+    if state.get("finished", False):
+        return END
+    else:
+        return "chatbot"
+
+
+@tool
+def add_to_order(item: str, modifiers: Iterable[str]) -> str:
+    """Adds the specified baked goods to the customer's order, including any modifiers.
+
+    Returns:
+      The updated order in progress.
+    """
+
+
+@tool
+def confirm_order() -> str:
+    """Asks the customer if the order is correct.
+
+    Returns:
+      The user's free-text response.
+    """
+
+
+@tool
+def get_order() -> str:
+    """Returns the users order so far. One item per line."""
+
+
+@tool
+def clear_order():
+    """Removes all items from the user's order."""
+
+
+@tool
+def place_order() -> int:
+    """Sends the order to the waiter for fulfillment.
+
+    Returns:
+      The estimated number of minutes until the order is ready.
+    """
+
+
+# Auto-tools will be invoked automatically by the ToolNode
+auto_tools = [get_menu]
+tool_node = ToolNode(auto_tools)
+
+# Order-tools will be handled by the order node.
+order_tools = [add_to_order, confirm_order, get_order, clear_order, place_order]
+
+# The LLM needs to know about all of the tools, so specify everything here.
+llm_with_tools = llm.bind_tools(auto_tools + order_tools)
+
+
 graph_builder = StateGraph(OrderState)
 
-# Add the nodes, including the new tool_node.
+# Nodes
 graph_builder.add_node("chatbot", chatbot_with_tools)
 graph_builder.add_node("human", human_node)
 graph_builder.add_node("tools", tool_node)
+graph_builder.add_node("ordering", order_node)
 
-# Chatbot may go to tools, or human.
+# Chatbot -> {ordering, tools, human, END}
 graph_builder.add_conditional_edges("chatbot", maybe_route_to_tools)
-# Human may go back to chatbot, or exit.
+# Human -> {chatbot, END}
 graph_builder.add_conditional_edges("human", maybe_exit_human_node)
 
-# Tools always route back to chat afterwards.
+# Tools (both kinds) always route back to chat afterwards.
 graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge("ordering", "chatbot")
 
 graph_builder.add_edge(START, "chatbot")
-graph_with_menu = graph_builder.compile()
+graph_with_order_tools = graph_builder.compile()
 
-Image(graph_with_menu.get_graph().draw_mermaid_png())
+config = {"recursion_limit": 100}
+
+state = graph_with_order_tools.invoke({"messages": []}, config)
